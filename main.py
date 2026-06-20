3ffd350ff6c1d9cd42d4a21796b6fedba652d762
jvarjan <j.varjan@email.cz>
2026-06-21 00:19:18 +0200

    create main.tokenize()

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from tokenizer import CharacterTokenizer
+from tokenizer import Tokenizer, CharacterTokenizer
+
+
+def tokenize(input_text: str, tokenizer: type[Tokenizer]) -> list[int]:
+    """
+        Encode ``input_text`` into token indices using the given tokenizer.
+        Works with any ``Tokenizer`` subclass (e.g. ``CharacterTokenizer``)
+        since it relies only on the abstract ``encode`` interface.
+        Args:
+            input_text: str - text to encode
+            tokenizer: type[Tokenizer] - a Tokenizer subclass to instantiate
+    """
+    return tokenizer(input_text).encode()
+
 
 def main():
     txt: str = 'The way you do anything is the way you do everything.'
-    t = CharacterTokenizer(txt)
-    dlist = t.encode()
-    print(t.char_map)
-    print(t.input_text)
-    print(dlist)
+    print(tokenize(txt, CharacterTokenizer))
     
 
     
